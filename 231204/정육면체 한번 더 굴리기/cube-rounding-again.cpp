8e1092807480b2bba3660f170f53944d9fb88d8a
Hyuntaek Lim <33558083+devLupin@users.noreply.github.com>
2023-12-04 16:42:56 +0900

    Title: 정육면체 한번 더 굴리기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 231204/정육면체 한번 더 굴리기/cube-rounding-again.cpp
+++ 231204/정육면체 한번 더 굴리기/cube-rounding-again.cpp
@@ -1,43 +1,42 @@
 #include <iostream>
 #include <queue>
+#include <cstring>
 using namespace std;
-using pii = pair<int,int>;
+using pii = pair<int, int>;
 
 const int SZ = 25, BOTTOM = 2;
-const int dy[] = {0,-1,0,1};
-const int dx[] = {1,0,-1,0};
+const int dy[] = { 0,1,0,-1 };
+const int dx[] = { 1,0,-1,0 };
 int board[SZ][SZ];
-int dice[6] = {1, 2, 6, 5, 4, 3};
+bool vis[SZ][SZ];
+int dice[6] = { 1, 2, 6, 5, 4, 3 };
 int dir = 0, ans;
-pii dice_pos = {0,0};
+pii dice_pos = { 0,0 };
 
 int n, m;
 
-bool oom(int y, int x) {return y < 0 || x < 0 || y > n || x > n;}
+bool oom(int y, int x) { return y < 0 || x < 0 || y > n || x > n; }
 
 void move_dice() {
     int tmp;
 
-    if(oom(dice_pos.first+dy[dir], dice_pos.second+dx[dir])) dir = (dir+2) % 4;
+    if (oom(dice_pos.first + dy[dir], dice_pos.second + dx[dir])) dir = (dir + 2) % 4;
 
-    dice_pos.first += dy[dir];
-    dice_pos.second += dx[dir];
-
-    if(dir == 0) {
+    if (dir == 0) {
         tmp = dice[5];
         dice[5] = dice[0];
         dice[0] = dice[4];
         dice[4] = dice[2];
         dice[2] = tmp;
     }
-    else if(dir == 1) {
-        tmp = dice[3];
-        dice[3] = dice[0];
-        dice[0] = dice[1];
-        dice[1] = dice[2];
+    else if (dir == 1) {
+        tmp = dice[1];
+        dice[1] = dice[0];
+        dice[0] = dice[3];
+        dice[3] = dice[2];
         dice[2] = tmp;
     }
-    else if(dir == 2) {
+    else if (dir == 2) {
         tmp = dice[4];
         dice[4] = dice[0];
         dice[0] = dice[5];
@@ -45,12 +44,15 @@ void move_dice() {
         dice[2] = tmp;
     }
     else {
-        tmp = dice[1];
-        dice[1] = dice[0];
-        dice[0] = dice[3];
-        dice[3] = dice[2];
+        tmp = dice[3];
+        dice[3] = dice[0];
+        dice[0] = dice[1];
+        dice[1] = dice[2];
         dice[2] = tmp;
     }
+
+    dice_pos.first += dy[dir];
+    dice_pos.second += dx[dir];
 }
 
 int adj() {
@@ -58,25 +60,27 @@ int adj() {
     int x = dice_pos.second;
 
     queue<pii> q;
-    bool vis[SZ][SZ] = {false,};
     int target = board[y][x];
     int ret = 1;
 
-    q.push({y,x});
+    for (int i = 0; i < SZ; i++)
+        memset(vis[i], false, sizeof(vis[i]));
+
+    q.push({ y,x });
     vis[y][x] = true;
 
-    while(!q.empty()) {
+    while (!q.empty()) {
         pii cur = q.front();
         q.pop();
 
-        for(int dir=0; dir<4; dir++) {
+        for (int dir = 0; dir < 4; dir++) {
             int ny = cur.first + dy[dir];
             int nx = cur.second + dx[dir];
 
-            if(oom(ny,nx)) continue;
-            if(vis[ny][nx] || target != board[ny][nx]) continue;
+            if (oom(ny, nx)) continue;
+            if (vis[ny][nx] || target != board[ny][nx]) continue;
 
-            q.push({ny,nx});
+            q.push({ ny,nx });
             vis[ny][nx] = true;
             ret++;
         }
@@ -88,8 +92,8 @@ int adj() {
 void set_dir() {
     int v = board[dice_pos.first][dice_pos.second];
 
-    if(dice[BOTTOM] > v) dir = (dir+3) % 4;
-    else if(dice[BOTTOM] < v) dir = (dir+1) % 4;
+    if (dice[BOTTOM] > v) dir = (dir + 1) % 4;
+    else if (dice[BOTTOM] < v) dir = (dir + 3) % 4;
 }
 
 int main() {
@@ -97,17 +101,17 @@ int main() {
     cin.tie(NULL);
 
     cin >> n >> m;
-    for(int i=0; i<n; i++)
-        for(int j=0; j<n; j++)
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
             cin >> board[i][j];
 
-    while(m--) {
+    while (m--) {
         move_dice();
         int num = adj();
         ans += (board[dice_pos.first][dice_pos.second] * num);
         set_dir();
     }
-
     cout << ans;
+
     return 0;
 }
