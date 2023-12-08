1e14d79b0730b4dab396b21e14dc926163ef6b44
Hyuntaek Lim <33558083+devLupin@users.noreply.github.com>
2023-12-08 13:35:15 +0900

    Title: 테트리스 블럭 안의 합 최대화 하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 231208/테트리스 블럭 안의 합 최대화 하기/max-sum-of-tetris-block.cpp
@@ -0,0 +1,51 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <bits/stdc++.h>
+#define Y first
+#define X second
+using namespace std;
+using pii = pair<int,int>;
+
+int n, m, ans, A[205][205];
+bool vis[205][205];
+
+const int dx[] = {-1,1,0,0};
+const int dy[] = {0,0,-1,1};
+
+void dfs(int x, int y, int cnt, int sum) {
+    if(cnt == 4) {
+        ans = max(ans, sum);
+        return;
+    }
+
+    for(int dir=0; dir<4; dir++) {
+        int nx = x + dx[dir];
+        int ny = y + dy[dir];
+
+        if(nx < 0 || ny < 0 || nx >= n || ny >= m) continue;
+        if(vis[nx][ny]) continue;
+
+        vis[nx][ny] = true;
+        dfs(nx, ny, cnt+1, sum+A[nx][ny]);
+        vis[nx][ny] = false;
+    }
+}
+
+int main(void) {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    // freopen("input.txt", "r", stdin);
+
+    cin >> n >> m;
+    for(int i=0; i<n; i++)
+        for(int j=0; j<m; j++)
+            cin >> A[i][j];
+
+    for(int i=0; i<n; i++)
+        for(int j=0; j<m; j++)
+            dfs(i, j, 0, 0);
+
+    cout << ans;
+    
+    return 0;
+}
