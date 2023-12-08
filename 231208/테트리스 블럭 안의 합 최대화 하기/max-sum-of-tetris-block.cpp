58a5a65fa4e1afc4b00508a6e6410363944803b6
Hyuntaek Lim <33558083+devLupin@users.noreply.github.com>
2023-12-08 13:43:49 +0900

    Title: 테트리스 블럭 안의 합 최대화 하기, Time: 98ms, Memory: 0MB, Status: Passed - CodeTree

--- 231208/테트리스 블럭 안의 합 최대화 하기/max-sum-of-tetris-block.cpp
+++ 231208/테트리스 블럭 안의 합 최대화 하기/max-sum-of-tetris-block.cpp
@@ -11,6 +11,8 @@ bool vis[205][205];
 const int dx[] = {-1,1,0,0};
 const int dy[] = {0,0,-1,1};
 
+bool oom(int x, int y) {return x < 0 || y < 0 || x >= n || y >= m;}
+
 void dfs(int x, int y, int cnt, int sum) {
     if(cnt == 4) {
         ans = max(ans, sum);
@@ -21,13 +23,40 @@ void dfs(int x, int y, int cnt, int sum) {
         int nx = x + dx[dir];
         int ny = y + dy[dir];
 
-        if(nx < 0 || ny < 0 || nx >= n || ny >= m) continue;
-        if(vis[nx][ny]) continue;
+        if(oom(nx, ny) || vis[nx][ny]) continue;
 
         vis[nx][ny] = true;
         dfs(nx, ny, cnt+1, sum+A[nx][ny]);
         vis[nx][ny] = false;
     }
+}
+
+void block(int x, int y) {
+    int x1, x2, x3, y1, y2, y3;
+
+    tie(x1, y1) = make_pair(x-1, y);
+    tie(x2, y2) = make_pair(x+1, y);
+    tie(x3, y3) = make_pair(x, y+1);
+    if(!oom(x1, y1) && !oom(x2, y2) && !oom(x3, y3))
+        ans = max(ans, A[x][y]+A[x1][y1]+A[x2][y2]+A[x3][y3]);
+
+    tie(x1, y1) = make_pair(x, y-1);
+    tie(x2, y2) = make_pair(x, y+1);
+    tie(x3, y3) = make_pair(x+1, y);
+    if(!oom(x1, y1) && !oom(x2, y2) && !oom(x3, y3))
+        ans = max(ans, A[x][y]+A[x1][y1]+A[x2][y2]+A[x3][y3]);
+
+    tie(x1, y1) = make_pair(x, y-1);
+    tie(x2, y2) = make_pair(x-1, y);
+    tie(x3, y3) = make_pair(x+1, y);
+    if(!oom(x1, y1) && !oom(x2, y2) && !oom(x3, y3))
+        ans = max(ans, A[x][y]+A[x1][y1]+A[x2][y2]+A[x3][y3]);
+    
+    tie(x1, y1) = make_pair(x, y-1);
+    tie(x2, y2) = make_pair(x, y+1);
+    tie(x3, y3) = make_pair(x-1, y);
+    if(!oom(x1, y1) && !oom(x2, y2) && !oom(x3, y3))
+        ans = max(ans, A[x][y]+A[x1][y1]+A[x2][y2]+A[x3][y3]);
 }
 
 int main(void) {
@@ -42,8 +71,10 @@ int main(void) {
             cin >> A[i][j];
 
     for(int i=0; i<n; i++)
-        for(int j=0; j<m; j++)
+        for(int j=0; j<m; j++) {
             dfs(i, j, 0, 0);
+            block(i, j);
+        }
 
     cout << ans;
     
