9638802c4c59af4818aff1dd2aab5b0dbc2d3c55
Hyuntaek Lim <33558083+devLupin@users.noreply.github.com>
2023-12-18 19:11:27 +0900

    Title: 돌아가는 팔각 의자, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 231218/돌아가는 팔각 의자/rounding-eight-angle.cpp
@@ -0,0 +1,82 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <bits/stdc++.h>
+using namespace std;
+using pii = pair<int, int>;
+
+const int sup[4] = { 1, 2, 4, 8 };
+const int left_adj = 6, right_adj = 2;
+deque<char> chair[4];
+int k;
+
+void rotate(vector<pii> stat) {
+	for (auto nxt : stat) {
+		int n = nxt.first;
+		int d = nxt.second;
+		auto cur = chair[n];
+		
+
+		if (d == -1) {
+			char tmp = cur.front();
+			cur.pop_front();
+			cur.push_back(tmp);
+		}
+		else {
+			char tmp = cur.back();
+			cur.pop_back();
+			cur.push_front(tmp);
+		}
+
+		chair[n] = cur;
+	}
+}
+
+void solution(int n, int d) {
+	vector<pii> stat;
+	int dir = d;
+
+	for (int i = n - 1; i >= 0; i--) {
+		if (chair[i + 1][left_adj] != chair[i][right_adj]) {
+			dir *= -1;
+			stat.push_back({ i, dir });
+		}
+	}
+
+	dir = d;
+	for (int i = n + 1; i < 4; i++) {
+		if (chair[i - 1][right_adj] != chair[i][left_adj]) {
+			dir *= -1;
+			stat.push_back({ i, dir });
+		}
+	}
+
+	rotate(stat);
+	rotate({ {n, d} });
+}
+
+int main(void) {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	// freopen("input.txt", "r", stdin);
+	for (int i = 0; i < 4; i++) {
+		string s;
+		cin >> s;
+		for (auto nxt : s) chair[i].push_back(nxt);
+	}
+	cin >> k;
+
+	for (int n, d; k--;) {
+		cin >> n >> d;
+		n--;
+
+		solution(n, d);
+	}
+
+	int ans = 0;
+	for (int i = 0; i < 4; i++)
+		if (chair[i][0] == '1')
+			ans += sup[i];
+	cout << ans;
+
+	return 0;
+}
