eda68ce620ec46a998eb1f3061b5f97532709248
Hyuntaek Lim <33558083+devLupin@users.noreply.github.com>
2024-01-17 17:39:04 +0900

    Title: 전투 로봇, Time: 290ms, Memory: 0MB, Status: Passed - CodeTree

--- 240117/전투 로봇/fighting-robot.cpp
+++ 240117/전투 로봇/fighting-robot.cpp
@@ -79,7 +79,7 @@ int main(void) {
 	ios::sync_with_stdio(false);
 	cin.tie(NULL);
 
-	freopen("input.txt", "r", stdin);
+	// freopen("input.txt", "r", stdin);
 
 	cin >> n;
 	for (int i = 0; i < n; i++) {
