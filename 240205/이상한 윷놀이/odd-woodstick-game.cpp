e9475239264a082cddc275679bf7421c9bc248c9
Hyuntaek Lim <33558083+devLupin@users.noreply.github.com>
2024-02-05 19:29:45 +0900

    Title: 이상한 윷놀이, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240205/이상한 윷놀이/odd-woodstick-game.cpp
+++ 240205/이상한 윷놀이/odd-woodstick-game.cpp
@@ -1,118 +1,76 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <bits/stdc++.h>
 using namespace std;
-using pii = pair<int, int>;
 
-int n, k, A[15][15];
+struct horse { int x, y, d; };
+horse h[10];
 
-const int dx[] = { 0,0,-1,1 };
-const int dy[] = { 1,-1,0,0 };
+int N, K;
+int dx[] = { 0, 0, 0, -1, 1 };
+int dy[] = { 0, 1, -1, 0, 0 };
+int turn[] = { 0, 2, 1, 4, 3 };
+int color[13][13];
+vector<int> info[13][13];
 
-struct info {
-	int x, y, d, o;
-};
-vector<info> v;
+bool oom(int x, int y) { return x <= 0 || y <= 0 || x > N || y > N; }
 
-bool oom(int x, int y) { return x < 0 || y < 0 || x >= n || y >= n; }
+int move(int i) {
+	auto& now = h[i];
+	int nx = now.x + dx[now.d];
+	int ny = now.y + dy[now.d];
 
-int change_dir(int d) {
-	int ret = -1;
-	if (d == 0) ret = 1;
-	else if (d == 1) ret = 0;
-	else if (d == 2) ret = 3;
-	else if (d == 3) ret = 2;
-	return ret;
-}
+	if (oom(nx, ny) || color[nx][ny] == 2) {
+		now.d = turn[now.d];
 
-void Reverse(vector<int> idx) {
-	vector<pii> order;
+		nx = now.x + dx[now.d];
+		ny = now.y + dy[now.d];
 
-	for (int& i : idx)
-		order.push_back({ v[i].o, i });
-
-	sort(order.begin(), order.end(), greater<>());
-	for (int i = 0; i < order.size(); i++) {
-		pii cur = order[i];
-		v[cur.second].o = i + 1;
+		if (oom(nx, ny) || color[nx][ny] == 2)
+			return 0;
 	}
+	vector<int>& cur = info[now.x][now.y];
+	vector<int>& next = info[nx][ny];
+	auto s = find(cur.begin(), cur.end(), i);
+
+	if (color[nx][ny] == 1)
+		reverse(s, cur.end());
+
+	for (auto it = s; it != cur.end(); ++it) {
+		h[*it].x = nx, h[*it].y = ny;
+		next.push_back(*it);
+	}
+
+	cur.erase(s, cur.end());
+	return next.size();
 }
 
-vector<int> move(info& cur, int nx, int ny) {
-	vector<int> idx;
-	int mx = 0, cnt = 0;
-
-	for (int i = 0; i < v.size(); i++) {
-		auto& nxt = v[i];
-		if (nxt.x == cur.x && nxt.y == cur.y && nxt.o >= cur.o)
-			idx.push_back(i);
-		else if (nxt.x == nx && nxt.y == ny) {
-			mx = max(mx, nxt.o);
-			cnt++;
+int simulation() {
+	for (int t = 1; t <= 1000; t++)	{
+		for (int i = 0; i < K; ++i) {
+			int cnt = move(i);
+			if (cnt >= 4) return t;
 		}
 	}
 
-	for (int& i : idx) {
-		v[i].x = nx;
-		v[i].y = ny;
-		v[i].o += mx;
-	}
-
-	idx.push_back(cnt);
-	return idx;
+	return -1;
 }
 
-bool go() {
-	for (auto& nxt : v) {
-		int d = nxt.d;
-		int nx = nxt.x + dx[d];
-		int ny = nxt.y + dy[d];
-
-		if (oom(nx, ny) || A[nx][ny] == 2) {
-			d = change_dir(d);
-			nx = nxt.x + dx[d];
-			ny = nxt.y + dy[d];
-
-			if (oom(nx, ny) || A[nx][ny] == 2) continue;
-			nxt.d = d;
-		}
-
-		vector<int> idx = move(nxt, nx, ny);
-		int cnt = idx.back();
-		idx.pop_back();
-
-		if ((int)idx.size() + cnt >= 4) return true;
-
-		if (A[nx][ny] == 1)
-			Reverse(idx);
-	}
-
-	return false;
-}
-
-int main(void)
-{
-	ios::sync_with_stdio(false);
-	cin.tie(NULL);
+int main() {
+	ios::sync_with_stdio(0); 
+	cin.tie(0);
 
 	// freopen("input.txt", "r", stdin);
-	cin >> n >> k;
 
-	for (int i = 0; i < n; i++)
-		for (int j = 0; j < n; j++)
-			cin >> A[i][j];
+	cin >> N >> K;
+	for (int i = 1; i <= N; ++i)
+		for (int j = 1; j <= N; ++j)
+			cin >> color[i][j];
 
-	for (int i = 0; i < k; i++) {
-		int x, y, d;
-		cin >> x >> y >> d;
-		x--, y--, d--;
-		v.push_back({ x,y,d,1 });
+	for (int i = 0; i < K; ++i)	{
+		horse& ho = h[i];
+		cin >> ho.x >> ho.y >> ho.d;
+		info[ho.x][ho.y].push_back(i);
 	}
-
-	int cnt = 0;
-	while (cnt++ <= 1000 && !go());
-
-	if (cnt > 1000) cout << -1;
-	else cout << cnt;
-
+	cout << simulation();
 	return 0;
 }
