424919d0e7c115370e258771ce48c4f8b1c826a3
Hyuntaek Lim <33558083+devLupin@users.noreply.github.com>
2024-02-06 15:53:35 +0900

    Title: 윷놀이 사기단, Time: 16ms, Memory: 0MB, Status: Passed - CodeTree

--- 240206/윷놀이 사기단/woodstick-fraud.cpp
+++ 240206/윷놀이 사기단/woodstick-fraud.cpp
@@ -8,43 +8,35 @@ int ans, board[SZ], route[SZ], turn[SZ], dice[10], piece[4];
 bool vis[SZ];
 
 void init() {
-	int num = 2;
-	for (int i = 1; i <= 20; i++) {
-		board[i] = num;
-		num += 2;
-		route[i] = i + 1;
-	}
-	route[0] = 1;
+	for (int i = 0; i < 21; i++) route[i] = i + 1;
 
-	board[21] = -1;  // 도착점
+	route[21] = 21;
+	for (int i = 22; i < 27; i++) route[i] = i + 1;
 
-	// 테두리 외의 값
-	board[22] = 13;
-	board[23] = 16;
+	route[27] = 20; 
+	route[28] = 29; 
+	route[29] = 30;
+	route[30] = 25; 
+	route[31] = 32; 
+	route[32] = 25;
+
+	turn[5] = 22; 
+	turn[10] = 31; 
+	turn[15] = 28;
+
+	for (int i = 0; i < 21; i++) board[i] = 2 * i;
+
+	board[22] = 13; 
+	board[23] = 16; 
 	board[24] = 19;
-
-	board[25] = 28;
-	board[26] = 27;
-	board[28] = 26;
-	
-	board[29] = 22;
-	board[30] = 24;
-	board[31] = 25;
-	board[32] = 30;
-	board[33] = 35;
-
-	for (int i = 22; i <= 33; i++)
-		route[i] = i + 1;
-	
-	// 테두리 외의 연결점
-	route[24] = 31;
-	route[28] = 31;
-	route[33] = 20;
-
-	// 파란점
-	turn[5] = 22;
-	turn[10] = 29;
-	turn[15] = 25;
+	board[25] = 25; 
+	board[26] = 30; 
+	board[27] = 35;
+	board[28] = 28; 
+	board[29] = 27; 
+	board[30] = 26;
+	board[31] = 22; 
+	board[32] = 24;
 }
 
 void solution(int depth, int sum) {
@@ -80,13 +72,13 @@ void solution(int depth, int sum) {
 }
 
 int main() {
-	ios::sync_with_stdio(0); 
+	ios::sync_with_stdio(0);
 	cin.tie(0);
 
 	// freopen("input.txt", "r", stdin);
 
 	for (int i = 0; i < 10; i++) cin >> dice[i];
-	
+
 	init();
 	solution(0, 0);
 
