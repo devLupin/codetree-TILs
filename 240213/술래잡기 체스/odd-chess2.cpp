0915b462d3a51182bc486b73b0fac97cc875ef97
Hyuntaek Lim <33558083+devLupin@users.noreply.github.com>
2024-02-13 22:48:30 +0900

    Title: 술래잡기 체스, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240213/술래잡기 체스/odd-chess2.cpp
+++ 240213/술래잡기 체스/odd-chess2.cpp
@@ -5,82 +5,114 @@ using pii = pair<int, int>;
 
 const int dx[] = { 0,-1,-1,0,1,1,1,0,-1 };
 const int dy[] = { 0,0,-1,-1,-1,0,1,1,1 };
-int ans;
 
-void print(vector<vector<pii>> board) {
-	for (int i = 0; i < 4; i++) {
-		for (int j = 0; j < 4; j++)
-			cout << board[i][j].first << ' ';
-		cout << '\n';
-	}
-	cout << '\n';
-}
+struct info {
+	int x, y, d;
+	bool alive;
+};
+vector<info> chess;
+vector<vector<int>> board;
+int ans;
 
 bool oom(int x, int y) { return x < 0 || y < 0 || x >= 4 || y >= 4; }
 
-void move(vector<vector<pii>>& board, int x, int y) {
-	int d = board[x][y].second;
-
-	int nx = x + dx[d];
-	int ny = y + dy[d];
-
-	while (oom(nx, ny) || board[nx][ny].first == -1) {
-		d++;
-		if (d == 9) d = 1;
-		nx = x + dx[d];
-		ny = y + dy[d];
-	}
-
-	board[x][y].second = d;
-	swap(board[x][y], board[nx][ny]);
+void swap_chess(int cur, int nxt) {
+	info tmp = chess[cur];
+	chess[cur].x = chess[nxt].x;
+	chess[cur].y = chess[nxt].y;
+	chess[nxt].x = tmp.x;
+	chess[nxt].y = tmp.y;
 }
 
-void move_chess(vector<vector<pii>>& board) {
-	int nxt = 1;
+void move_chess() {
+	for (int i = 1; i <= 16; i++) {
+		if (!chess[i].alive) continue;
 
-	while (nxt <= 16) {
-		bool flag = true;
+		auto& cur = chess[i];
 
-		for (int i = 0; i < 4 && flag; i++) {
-			for (int j = 0; j < 4; j++) {
-				if (board[i][j].first == nxt) {
-					flag = false;
-					move(board, i, j);
-					break;
-				}
+		int x = cur.x;
+		int y = cur.y;
+		int d = cur.d;
+		int nx = x + dx[d];
+		int ny = y + dy[d];
+
+		bool flag = false;
+
+		if (!oom(nx, ny)) {
+			if (board[nx][ny] == 0) {
+				flag = true;
+				cur = { nx, ny, d };
+				swap(board[x][y], board[nx][ny]);
+			}
+			else if (board[nx][ny] != -1) {
+				flag = true;
+				swap_chess(i, board[nx][ny]);
+				swap(board[x][y], board[nx][ny]);
 			}
 		}
 
-		nxt++;
+		if (!flag) {
+			int nd = d + 1;
+			if (nd == 9) nd = 1;
+
+			nx = x + dx[nd];
+			ny = y + dy[nd];
+
+			while (nd != d) {
+				if (!oom(nx, ny)) {
+					if (board[nx][ny] == 0) {
+						cur = { nx, ny, nd };
+						swap(board[x][y], board[nx][ny]);
+						break;
+					}
+					else if (board[nx][ny] != -1) {
+						swap_chess(i, board[nx][ny]);
+						swap(board[x][y], board[nx][ny]);
+						chess[i].d = nd;
+						break;
+					}
+				}
+
+				nd++;
+				if (nd == 9) nd = 1;
+
+				nx = x + dx[nd];
+				ny = y + dy[nd];
+			}
+		}
 	}
 }
 
-void solve(vector<vector<pii>> board, int x, int y, int d, int sum) {
-	vector<pii> pos;
-
-	for (int i = 1; i < 4; i++) {
-		int nx = x + dx[d] * i;
-		int ny = y + dy[d] * i;
-
-		if (oom(nx, ny) || board[nx][ny].first == -1) continue;
-		pos.push_back({ nx, ny });
-	}
-
-	vector<vector<pii>> undo(board.begin(), board.end());
+void solve(int x, int y, int dir, int sum) {
 	ans = max(ans, sum);
 
-	for (auto& nxt : pos) {
-		int nx = nxt.first;
-		int ny = nxt.second;
-		int val = board[nx][ny].first;
-		int dir = board[nx][ny].second;
+	vector<vector<int>> cpy_board(board.begin(), board.end());
+	vector<info> cpy_chess(chess.begin(), chess.end());
+	move_chess();
 
-		board[nx][ny].first = -1;
-		move_chess(board);
-		solve(board, nx, ny, dir, sum + val);
-		board[nx][ny].first = val;
-		board = undo;
+	for (int i = 1; i < 4; i++) {
+		int nx = x + dx[dir] * i;
+		int ny = y + dy[dir] * i;
+
+		if (oom(nx, ny)) break;
+		if (board[nx][ny] == 0) continue;
+
+		int num = board[nx][ny];
+		int nd = chess[num].d;
+
+		board[x][y] = 0;
+		board[nx][ny] = -1;
+		chess[num].alive = false;
+
+		solve(nx, ny, nd, sum + num);
+
+		board[x][y] = -1;
+		board[nx][ny] = num;
+		chess[num].alive = true;
 	}
+
+	chess = cpy_chess;
+	board = cpy_board;
 }
 
 int main(void) {
@@ -89,24 +121,24 @@ int main(void) {
 
 	// freopen("input.txt", "r", stdin);
 
-	vector<vector<pii>> board;
+	chess.assign(17, {});
 	board.assign(4, {});
 
 	for (int i = 0; i < 4; i++) {
 		for (int j = 0; j < 4; j++) {
 			int p, d;
 			cin >> p >> d;
-
-			board[i].push_back({ p,d });
+			chess[p] = { i, j, d, true };
+			board[i].push_back(p);
 		}
 	}
 
-	ans = board[0][0].first;
-	board[0][0].first = -1;
-	move_chess(board);
+	int num = board[0][0];
+	int d = chess[num].d;
+	chess[num].alive = false;
+	board[0][0] = -1;
 
-	solve(board, 0, 0, board[0][0].second, ans);
-
+	solve(0, 0, d, num);
 	cout << ans;
 
 	return 0;
