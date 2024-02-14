e6bd2d156d45d74c52438d0b948687e593232a42
Hyuntaek Lim <33558083+devLupin@users.noreply.github.com>
2024-02-14 15:21:27 +0900

    Title: 승자독식 모노폴리, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240214/승자독식 모노폴리/odd-monopoly.cpp
@@ -0,0 +1,184 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <bits/stdc++.h>
+#define X first
+#define Y second
+using namespace std;
+using pii = pair<int, int>;
+
+int N, M, K, p, d, d1, d2, d3, d4;
+
+const int SZ = 25;
+const int dx[] = { 0,-1,1,0,0 };
+const int dy[] = { 0,0,0,-1,1 };
+
+struct info {
+	bool alive;
+	int dir;
+	vector<vector<int>> dirs;
+};
+
+pii pos[SZ * SZ];
+pii board[SZ][SZ];
+vector<info> players;
+vector<pii> nxt_board[SZ][SZ];
+
+void input() {
+	cin >> N >> M >> K;
+	for (int i = 1; i <= N; i++) {
+		for (int j = 1; j <= N; j++) {
+			cin >> p;
+			if (p > 0) {
+				pos[p] = { i, j };
+				board[i][j] = { p, K };
+			}
+			else board[i][j] = { 0,0 };
+		}
+	}
+
+	players.assign(M + 1, {});
+	for (int i = 1; i <= M; i++) {
+		cin >> d;
+		players[i].alive = true;
+		players[i].dir = d;
+	}
+
+	for (int i = 1; i <= M; i++) {
+		players[i].dirs.assign(5, vector<int>(5));
+
+		for (int j = 1; j <= 4; j++) {
+			cin >> d1 >> d2 >> d3 >> d4;
+			players[i].dirs[j] = { 0, d1, d2, d3, d4 };
+		}
+	}
+}
+
+bool is_alive() {
+	for (int i = 2; i <= M; i++)
+		if (players[i].alive) return false;
+	return true;
+}
+
+int nxt_dir(int i, int target) {
+	info& cur = players[i];
+	int x = pos[i].X;
+	int y = pos[i].Y;
+
+	for (int dir = 1; dir <= 4; dir++) {
+		int nd = cur.dirs[d][dir];
+		int nx = x + dx[nd];
+		int ny = y + dy[nd];
+
+		if (nx < 0 || ny < 0 || nx >= N || ny >= N) continue;
+		if (board[nx][ny].first == target) return nd;
+	}
+
+	return -1;
+}
+
+void reduce_turn() {
+	for (int i = 1; i <= N; i++) {
+		for (int j = 1; j <= N; j++) {
+			if (board[i][j].second > 0) {
+				board[i][j].second--;
+				if (board[i][j].second == 0)
+					board[i][j] = { 0,0 };
+			}
+		}
+	}
+}
+
+bool compare(const pii& a, const pii& b) {
+	return a.first < b.first;
+}
+
+bool oom(int x, int y) { return x < 1 || y < 1 || x > N || y > N; }
+
+void kill_player() {
+	for (int i = 1; i <= N; i++) {
+		for (int j = 1; j <= N; j++) {
+			auto& nxt = nxt_board[i][j];
+			if (nxt.size() == 1) {
+				board[i][j] = nxt[0];
+				pos[nxt[0].first] = { i, j };
+			}
+			else if (nxt.size() > 1) {
+				sort(nxt.begin(), nxt.end());
+				board[i][j] = nxt[0];
+				pos[nxt[0].first] = { i, j };
+
+				for (int idx = 1; idx < nxt.size(); idx++) {
+					int num = nxt[idx].first;
+					players[num].alive = false;
+				}
+			}
+		}
+	}
+}
+
+void move_player() {
+	for (int i = 1; i <= M; i++) {
+		if (!players[i].alive) continue;
+
+		info& cur = players[i];
+		int d = cur.dir;
+		int nx = pos[i].X + dx[d];
+		int ny = pos[i].Y + dy[d];
+
+		if (!oom(nx, ny) && board[nx][ny].first == 0) {
+			nxt_board[nx][ny].push_back({ i, K });
+			continue;
+		}
+
+		d = nxt_dir(i, 0);
+		if (d == -1) d = nxt_dir(i, i);
+
+		nx = pos[i].X + dx[d];
+		ny = pos[i].Y + dy[d];
+
+		nxt_board[nx][ny].push_back({ i, K });
+		cur.dir = d;
+	}
+}
+
+void print() {
+	for (int i = 1; i <= N; i++) {
+		for (int j = 1; j <= N; j++) {
+			cout << board[i][j].first << '(' << board[i][j].second << ')' << ' ';
+		}
+		cout << '\n';
+	}
+	cout << '\n';
+}
+
+void init() {
+	for (int i = 1; i <= N; i++)
+		for (int j = 1; j <= N; j++)
+			nxt_board[i][j].clear();
+}
+
+int main(void) {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	// freopen("input.txt", "r", stdin);
+	input();
+
+	int ans = -1;
+	for(int t=0; t<=1000; t++) {
+		if (!players[1].alive) break;
+		if (is_alive()) {
+			ans = t;
+			break;
+		}
+
+		init();
+
+		move_player();
+		reduce_turn();
+		kill_player();
+	}
+
+	cout << ans;
+
+	return 0;
+}
