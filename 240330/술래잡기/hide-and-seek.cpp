b7c40c3d2e3744381c104c61624ecccb2c548c7a
Hyuntaek Lim <33558083+devLupin@users.noreply.github.com>
2024-03-30 23:22:31 +0900

    Title: 술래잡기, Time: 14ms, Memory: 9MB, Status: Passed - CodeTree

--- 240330/술래잡기/hide-and-seek.cpp
+++ 240330/술래잡기/hide-and-seek.cpp
@@ -1,17 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <bits/stdc++.h>
 #define X first
 #define Y second
 using namespace std;
-using pii = pair<int,int>;
+using pii = pair<int, int>;
 
 const int SZ = 100;
 const int DIR_NUM = 4;
-const int dx[] = {0,0,1,-1};
-const int dy[] = {-1,1,0,0};
-const int fx[] = {-1,0,1,0};
-const int fy[] = {0,1,0,-1};
-const int bx[] = {1,0,-1,0};
-const int by[] = {0,1,0,-1};
+const int dx[] = { 0,0,1,-1 };
+const int dy[] = { -1,1,0,0 };
+const int fx[] = { -1,0,1,0 };
+const int fy[] = { 0,1,0,-1 };
+const int bx[] = { 1,0,-1,0 };
+const int by[] = { 0,1,0,-1 };
 
 int runner[SZ + 1][SZ][SZ][DIR_NUM];
 int N, M, H, K, board[SZ][SZ], ans;
@@ -24,11 +25,11 @@ int Distance(int x, int y) { return abs(tx - x) + abs(ty - y); }
 bool OOM(int x, int y) { return x < 0 || y < 0 || x >= N || y >= N; }
 
 void MoveRunner(int t) {
-	for(int i=0; i<N; i++) {
-		for(int j=0; j<N; j++) {
-			for(int k=0; k<DIR_NUM; k++) {
-				if(runner[t - 1][i][j][k] > 0) {
-					if(Distance(i, j) > 3) {
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			for (int k = 0; k < DIR_NUM; k++) {
+				if (runner[t - 1][i][j][k] > 0) {
+					if (Distance(i, j) > 3) {
 						runner[t][i][j][k] += runner[t - 1][i][j][k];
 						continue;
 					}
@@ -37,48 +38,37 @@ void MoveRunner(int t) {
 					int nx = i + dx[d];
 					int ny = j + dy[d];
 
-					if(OOM(nx, ny)) {
-						if(d == 0) d = 1;
-						if(d == 1) d = 0;
-						if(d == 2) d = 3;
-						if(d == 3) d = 2;
+					if (OOM(nx, ny)) {
+						if (d == 0) d = 1;
+						else if (d == 1) d = 0;
+						else if (d == 2) d = 3;
+						else if (d == 3) d = 2;
 					}
 
 					nx = i + dx[d];
 					ny = j + dy[d];
 
-					if(nx == tx && ny == ty) runner[t][i][j][k] += runner[t - 1][i][j][k];
+					if (nx == tx && ny == ty) runner[t][i][j][k] += runner[t - 1][i][j][k];
 					else runner[t][nx][ny][d] += runner[t - 1][i][j][k];
 				}
 			}
 		}
 	}
-
-	// for(int i=0; i<N; i++) {
-	// 	for(int j=0; j<N; j++) {
-	// 		for(int k=0; k<DIR_NUM; k++) {
-	// 			if(runner[t][i][j][k] > 0) {
-	// 				cout << i << ' ' << j << ' ' << k << '\n';
-	// 			}
-	// 		}
-	// 	}
-	// }
-	// cout << "\n";
 }
 
 void MoveTagger() {
-	if(!flag) {
+	if (!flag) {
 		tx += fx[tDir];
 		ty += fy[tDir];
 
 		cnt++;
-		if(cnt == dirCnt) {
+		if (cnt == dirCnt) {
 			tDir = (tDir + 1) % 4;
 
 			cnt = 0;
 			dirChanged++;
 
-			if(dirChanged == 2) {
+			if (dirChanged == 2) {
 				dirCnt++;
 				dirChanged = 0;
 			}
@@ -89,24 +79,24 @@ void MoveTagger() {
 		ty += by[tDir];
 
 		cnt--;
-		if(cnt == 0) {
+		if (cnt == 0) {
 			tDir = (tDir + 1) % 4;
 
 			cnt = dirCnt;
 			dirChanged++;
 
-			if(dirChanged == 2) {
+			if (dirChanged == 2) {
 				dirCnt--;
 				dirChanged = 0;
 			}
 		}
 	}
 
-	if((tx == 0 && ty == 0) || (tx == N / 2 && ty == N / 2)) {
+	if ((tx == 0 && ty == 0) || (tx == N / 2 && ty == N / 2)) {
 		flag = !flag;
 		tDir = 0;
 
-		if(!flag) {
+		if (!flag) {
 			cnt = 0;
 			dirCnt = 1;
 			dirChanged = 0;
@@ -122,14 +112,14 @@ void Catch(int t) {
 	vector<int> ddx(4), ddy(4);
 	int sum = 0;
 
-	if(!flag) {
-		for(int i=0; i<4; i++) {
+	if (!flag) {
+		for (int i = 0; i < 4; i++) {
 			ddx[i] = fx[i];
 			ddy[i] = fy[i];
 		}
 	}
 	else {
-		for(int i=0; i<4; i++) {
+		for (int i = 0; i < 4; i++) {
 			ddx[i] = bx[i];
 			ddy[i] = by[i];
 		}
@@ -138,9 +128,9 @@ void Catch(int t) {
 	int nx = tx;
 	int ny = ty;
 
-	for(int i=0; i<3; i++) {
-		for(int k=0; k<4; k++) {
-			if(runner[t][nx][ny][k] > 0 && board[nx][ny] != 1) {
+	for (int i = 0; i < 3; i++) {
+		for (int k = 0; k < 4; k++) {
+			if (runner[t][nx][ny][k] > 0 && board[nx][ny] != 1) {
 				sum += runner[t][nx][ny][k];
 				runner[t][nx][ny][k] = 0;
 			}
@@ -150,16 +140,19 @@ void Catch(int t) {
 	}
 
 	ans += sum * t;
+}
 
-	// for(int i=0; i<N; i++) {
-	// 	for(int j=0; j<N; j++) {
-	// 		for(int k=0; k<DIR_NUM; k++) {
-	// 			if(runner[t][i][j][k] > 0) {
-	// 				cout << i << ' ' << j << ' ' << k << '\n';
-	// 			}
-	// 		}
-	// 	}
-	// }
+void Print(int t) {
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			for (int k = 0; k < DIR_NUM; k++) {
+				if (runner[t][i][j][k] > 0) {
+					cout << i + 1 << ' ' << j + 1 << ' ' << runner[t][i][j][k] << '\n';
+				}
+			}
+		}
+	}
+	cout << "\n";
 }
 
 int main(void) {
@@ -171,17 +164,17 @@ int main(void) {
 	cin >> N >> M >> H >> K;
 	tie(tx, ty) = make_pair(N / 2, N / 2);
 
-	for(int x, y, d; M--;) {
+	for (int x, y, d; M--;) {
 		cin >> x >> y >> d;
 		runner[0][x - 1][y - 1][d]++;
 	}
 
-	for(int x, y; H--;) {
+	for (int x, y; H--;) {
 		cin >> x >> y;
 		board[x - 1][y - 1] = 1;
 	}
 
-	for(int t=1; t<=K; t++) {
+	for (int t = 1; t <= K; t++) {
 		MoveRunner(t);
 		MoveTagger();
 		Catch(t);
