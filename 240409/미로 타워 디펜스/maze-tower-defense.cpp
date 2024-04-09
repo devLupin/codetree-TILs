4d6d51a4290a05c4924d6f9eb8439e9d7fee76bf
Hyuntaek Lim <33558083+devLupin@users.noreply.github.com>
2024-04-09 10:26:27 +0900

    Title: 미로 타워 디펜스, Time: 9ms, Memory: 0MB, Status: Passed - CodeTree

--- 240409/미로 타워 디펜스/maze-tower-defense.cpp
+++ 240409/미로 타워 디펜스/maze-tower-defense.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-const int SZ = 25;
+const int SZ = 30;
 const int dx[] = { 0,1,0,-1 };
 const int dy[] = { 1,0,-1,0 };
 const int ddx[] = { 0,1,0,-1 };
