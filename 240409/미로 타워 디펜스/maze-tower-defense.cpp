4f6962a5160aa72fbab55b93e726341139329835
Hyuntaek Lim <33558083+devLupin@users.noreply.github.com>
2024-04-09 10:23:56 +0900

    Title: 미로 타워 디펜스, Time: 16ms, Memory: 0MB, Status: Runtime Error - CodeTree

--- 240409/미로 타워 디펜스/maze-tower-defense.cpp
+++ 240409/미로 타워 디펜스/maze-tower-defense.cpp
@@ -1,17 +1,11 @@
-#define _CRT_SECURE_NO_WARNINGS
 #include <bits/stdc++.h>
-#define X first
-#define Y second
 using namespace std;
-using pii = pair<int, int>;
 
 const int SZ = 25;
-
 const int dx[] = { 0,1,0,-1 };
 const int dy[] = { 1,0,-1,0 };
 const int ddx[] = { 0,1,0,-1 };
 const int ddy[] = { -1,0,1,0 };
-
 int N, M, board[SZ][SZ];
 int sx, sy, ans;
 
@@ -31,11 +25,7 @@ vector<int> GetElem() {
 	vector<int> v;
 
 	auto [x, y] = make_pair(sx, sy);
-
-	int d = 0;
-	int cnt = 0;
-	int cut = 1;
-	int dirCnt = 0;
+	auto [d, cnt, cut, dirCnt] = make_tuple(0, 0, 1, 0);
 
 	while (true) {
 		if (x == 0 && y == 0) break;
@@ -84,6 +74,10 @@ vector<int> RemoveDup(vector<int> v) {
 
 			ret.push_back(cur);
 		}
+		if (cnt >= 4) {
+			ans += cnt * ret.back();
+			while (cnt--)ret.pop_back();
+		}
 
 		if (v.size() == ret.size()) break;
 		v = ret;
@@ -125,13 +119,8 @@ void Fill() {
 	fill(&board[0][0], &board[N][N], 0);
 
 	int len = min(N * N - 1, (int)v.size());
-
 	auto [x, y] = make_pair(sx, sy);
-
-	int d = 0;
-	int cnt = 0;
-	int cut = 1;
-	int dirCnt = 0;
+	auto [d, cnt, cut, dirCnt] = make_tuple(0, 0, 1, 0);
 
 	for (int i = 0; i < len; i++) {
 		x += ddx[d];
@@ -156,8 +145,6 @@ void Fill() {
 int main(void) {
 	ios::sync_with_stdio(false);
 	cin.tie(NULL);
-
-	// freopen("input.txt", "r", stdin);
 
 	cin >> N >> M;
 	for (int i = 0; i < N; i++)
