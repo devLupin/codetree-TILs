b8b74dd7852c57f35f4d292d1d9503da43bddeed
Hyuntaek Lim <33558083+devLupin@users.noreply.github.com>
2024-09-02 23:25:09 +0900

    Title: 고대 문명 유적 탐사, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

--- 240902/고대 문명 유적 탐사/ancient-ruin-exploration.cpp
+++ 240902/고대 문명 유적 탐사/ancient-ruin-exploration.cpp
@@ -1,171 +1,154 @@
 #define _CRT_SECURE_NO_WARNINGS
-#include <bits/stdc++.h>
+#include <queue>
+#include <iostream>
+#include <algorithm>
 #define X first
 #define Y second
 using namespace std;
 using pii = pair<int, int>;
-using vvi = vector<vector<int>>;
 
 const int dx[] = { -1,1,0,0 };
 const int dy[] = { 0,0,-1,1 };
 
-int K, M, fCnt, ans;
-vvi board;
-vector<int> filler;
+int K, M;
 
-void print(vvi cur)
-{
-	for (int i = 0; i < 5; i++)
-	{
-		for (int j = 0; j < 5; j++)
-			cout << cur[i][j] << ' ';
-		cout << '\n';
+class Board {
+	vector<vector<int>> map, nxt;
+	queue<int> art;
+
+public:
+	Board() { map.assign(5, vector<int>(5, 0)); }
+
+	void print() {
+		for (int i = 0; i < 5; i++) {
+			for (int j = 0; j < 5; j++)
+				cout << nxt[i][j] << ' ';
+			cout << '\n';
+		}
+		cout << "\n\n";
 	}
-	cout << "\n\n";
-}
 
-vvi rotate90(vvi cur, int sx, int sy)
-{
-	vvi nxt = cur;
-	for (int x = sx; x < sx + 3; x++) {
-		for (int y = sy; y < sy + 3; y++) {
+	void input() {
+		for (int i = 0; i < 5; i++)
+			for (int j = 0; j < 5; j++)
+				cin >> map[i][j];
 
-			int nx = y - sy;
-			int ny = 3 - (x - sx) - 1;
-
-			nxt[nx + sx][ny + sy] = cur[x][y];
+		for (int x, i = 0; i < M; i++) {
+			cin >> x;
+			art.push(x);
 		}
 	}
-	return nxt;
-}
 
-bool oom(int x, int y) { return x < 0 || y < 0 || x >= 5 || y >= 5; }
+	void rotate(int sx, int sy, int sz) {
+		auto tmp = nxt;
 
-vector<pii> bfs(vvi cboard)
-{
-	vector<pii> res;
-	bool vis[5][5] = { false, };
+		for (int x = sx; x < sx + sz; x++) {
+			for (int y = sy; y < sy + sz; y++) {
+				int nx = y - sy;
+				int ny = sz - (x - sx) - 1;
+				tmp[nx + sx][ny + sy] = nxt[x][y];
+			}
+		}
 
-	for (int i = 0; i < 5; i++) 
-	{
-		for (int j = 0; j < 5; j++) 
-		{
-			if (vis[i][j]) continue;
+		nxt = tmp;
+	}
 
-			vector<pii> v;
-			queue<pii> q;
+	int BFS() {
+		bool vis[5][5] = { false, };
+		int ret = 0;
 
-			q.push({ i,j });
-			vis[i][j] = true;
-			v.push_back({ i,j });
+		for (int x = 0; x < 5; x++)
+			for (int y = 0; y < 5; y++)
+				if (!vis[x][y] && nxt[x][y]) {
+					vector<pii> v;
+					queue<pii> q;
+					int cmp = nxt[x][y];
 
-			while (!q.empty())
-			{
-				pii cur = q.front();
-				q.pop();
+					q.push({ x, y });
+					vis[x][y] = true;
 
-				for (int dir = 0; dir < 4; dir++) 
-				{
-					int nx = cur.X + dx[dir];
-					int ny = cur.Y + dy[dir];
+					while (!q.empty()) {
+						pii cur = q.front();
+						q.pop();
 
-					if (!oom(nx, ny) && !vis[nx][ny] && cboard[i][j] == cboard[nx][ny])
-					{
-						q.push({ nx, ny });
-						vis[nx][ny] = true;
-						v.push_back({ nx,ny });
+						v.push_back(cur);
+
+						for (int dir = 0; dir < 4; dir++) {
+							int nx = cur.X + dx[dir];
+							int ny = cur.Y + dy[dir];
+
+							if (nx < 0 || ny < 0 || nx >= 5 || ny >= 5) continue;
+							if (vis[nx][ny] || nxt[nx][ny] != cmp) continue;
+
+							q.push({ nx, ny });
+							vis[nx][ny] = true;
+						}
+					}
+
+					if (v.size() >= 3) {
+						for (pii& pos : v) nxt[pos.X][pos.Y] = 0;
+						ret += v.size();
 					}
 				}
-			}
 
-			if (v.size() >= 3)
-				for (const pii& p : v)
-					res.push_back(p);
-		}
+		return ret;
 	}
 
-	return res;
-}
-
-vector<pii> UpdateBoard()
-{
-	vvi best = board;
-	auto bRes = vector<pii>();
-
-	for (int j = 0; j < 3; j++)
-	{
-		for (int i = 0; i < 3; i++)
-		{
-			for (int k = 1; k <= 3; k++)
-			{
-				vvi cmp = board;
-
-				for (int l = 0; l < K; l++)
-					cmp = rotate90(cmp, i, j);
-
-				auto cRes = bfs(cmp);
-				if (cRes.size() > bRes.size())
-				{
-					bRes = cRes;
-					best = cmp;
+	void fill() {
+		for (int y = 0; y < 5; y++)
+			for (int x = 4; x >= 0; x--)
+				if (nxt[x][y] == 0) {
+					nxt[x][y] = art.front();
+					art.pop();
 				}
-			}
-		}
 	}
 
-	board = best;
-	return bRes;
-}
+	int select() {
+		vector<vector<int>> best = map;
+		int cmp = 0;
 
-bool compare(const pii& a, const pii& b)
-{
-	if (a.Y != b.Y) return a.Y < b.Y;
-	return a.X > b.X;
-}
+		for (int i = 1; i <= 3; i++)
+			for (int y = 0; y < 3; y++)
+				for (int x = 0; x < 3; x++) {
+					nxt = map;
 
-void fill(vector<pii> res)
-{
-	sort(res.begin(), res.end(), compare);
+					for (int j = 0; j < i; j++)
+						rotate(x, y, 3);
 
-	for (const auto& nxt : res)
-	{
-		board[nxt.X][nxt.Y] = filler[fCnt++];
-		ans++;
+					int tmp = BFS();
+					if (cmp < tmp) {
+						best = nxt;
+						cmp = tmp;
+					}
+				}
+
+		nxt = best;
+		return cmp;
 	}
-}
 
-int main(void) 
-{
-	ios::sync_with_stdio(false);
-	cin.tie(NULL);
+	void done() { map = nxt; }
+};
 
+int main(void) {
 	// freopen("input.txt", "r", stdin);
-	board.assign(5, vector<int>(5));
-
 	cin >> K >> M;
 
-	for (int i = 0; i < 5; i++)
-		for (int j = 0; j < 5; j++)
-			cin >> board[i][j];
-	for (int x, i = 0; i < M; i++) 
-	{
-		cin >> x;
-		filler.push_back(x);
-	}
+	Board board;
+	board.input();
 
-	while (K--)
-	{
-		auto res = UpdateBoard();
-		if (res.empty()) break;
+	while (K--) {
+		int ans = board.select();
+		if (ans == 0) break;
 
-		while (true)
-		{
-			fill(res);
-			res = bfs(board);
-			if (res.empty()) break;
+		while (true) {
+			board.fill();
+			int new_ans = board.BFS();
+			if (new_ans == 0) break;
+			ans += new_ans;
 		}
+		cout << ans << " ";
+		board.done();
 	}
-	
-	cout << ans;
+
 	return 0;
 }
