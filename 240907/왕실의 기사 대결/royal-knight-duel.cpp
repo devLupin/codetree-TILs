3912e8ef69ddd96ea752da6c3b914cfb14aff9d9
Hyuntaek Lim <33558083+devLupin@users.noreply.github.com>
2024-09-07 19:02:25 +0900

    Title: 왕실의 기사 대결, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240907/왕실의 기사 대결/royal-knight-duel.cpp
+++ 240907/왕실의 기사 대결/royal-knight-duel.cpp
@@ -62,19 +62,6 @@ set<int> CanGo(int n, int dir)
 			q.push({ nx, ny });
 			vis[nx][ny] = true;
 		}
-
-		for (int ddir = 0; ddir < 4; ddir++)
-		{
-			if (dir == ddir) continue;
-			nx = cur.X + dx[ddir];
-			ny = cur.Y + dy[ddir];
-
-			if (!OOM(nx, ny) && !vis[nx][ny] && board[nx][ny] == board[cur.X][cur.Y])
-			{
-				q.push({ nx, ny });
-				vis[nx][ny] = true;
-			}
-		}
 	}
 
 	return node;
