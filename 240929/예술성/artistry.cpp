f90ef5929443d498e5079a34be2521a7d2707834
Hyuntaek Lim <33558083+devLupin@users.noreply.github.com>
2024-09-29 16:34:35 +0900

    Title: 예술성, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240929/예술성/artistry.cpp
+++ 240929/예술성/artistry.cpp
@@ -4,6 +4,7 @@
  *
  * @submit         01:01:43
  * @revision       00:03:16
+ * @revision       00:17:47
  */
 
 
@@ -16,171 +17,102 @@ using namespace std;
 using pii = pair<int, int>;
 using tiii = tuple<int, int, int>;
 
-struct pair_hash
-{
-	template <class T1, class T2>
-	std::size_t operator() (const std::pair<T1, T2>& pair) const {
-		return std::hash<T1>()(pair.first) ^ std::hash<T2>()(pair.second);
-	}
-};
-
+const int MAX_N = 30;
 const int dx[] = { -1,1,0,0 };
 const int dy[] = { 0,0,-1,1 };
-const int MAX_N = 30;
 
 int N, ans;
-int board[MAX_N][MAX_N], nxtBoard[MAX_N][MAX_N];
-bool vis[MAX_N][MAX_N];
+int board[MAX_N][MAX_N], nextBoard[MAX_N][MAX_N];
 int groupBoard[MAX_N][MAX_N];
-vector<int> numOfCells;
-vector<int> groupNum;
-unordered_map<pii, int, pair_hash> numAdj;
+bool vis[MAX_N][MAX_N];
+vector<int> gnum;
 
-void Init()
-{
+void Init() {
+	fill(&groupBoard[0][0], &groupBoard[N][N], 0);
 	fill(&vis[0][0], &vis[N][N], false);
-	fill(&groupBoard[0][0], &groupBoard[N][N], false);
-	numOfCells.clear();
-	groupNum.clear();
-	numAdj.clear();
+	gnum.clear();
 }
 
-bool OOM(int x, int y) { return x < 0 || y < 0 || x >= N || y >= N; }
-
-void MakeGroup(int x, int y)
-{
+void MakeGroup() {
 	int cnt = 0;
-	queue<pii> q;
-	q.push(make_pair(x, y));
 
-	while (!q.empty())
-	{
-		auto [cx, cy] = q.front();
-		q.pop();
+	for (int i = 0; i < N; i++)
+		for (int j = 0; j < N; j++)
+			if (!vis[i][j]) {
+				queue<pii> q;
+				int val = board[i][j];
 
-		groupBoard[cx][cy] = groupNum.size();
-		vis[cx][cy] = true;
-		cnt++;
+				gnum.push_back(0);
 
-		for (int dir = 0; dir < 4; dir++)
-		{
-			int nx = cx + dx[dir];
-			int ny = cy + dy[dir];
+				q.push({ i,j });
+				vis[i][j] = true;
 
-			if (!OOM(nx, ny) && !vis[nx][ny] && board[x][y] == board[nx][ny])
-			{
-				q.push(make_pair(nx, ny));
-				vis[nx][ny] = true;
-			}
-		}
-	}
+				while (!q.empty()) {
+					pii cur = q.front();
+					q.pop();
 
-	groupNum.push_back(board[x][y]);
-	numOfCells.push_back(cnt);
-}
+					groupBoard[cur.X][cur.Y] = cnt;
+					gnum[cnt]++;
 
-void MakeAdj(int x, int y) {
-	int target = groupBoard[x][y];
-	queue<pii> q;
+					for (int dir = 0; dir < 4; dir++) {
+						int nx = cur.X + dx[dir];
+						int ny = cur.Y + dy[dir];
 
-	q.push(make_pair(x, y));
-	vis[x][y] = true;
+						if (nx < 0 || ny < 0 || nx >= N || ny >= N) continue;
+						if (vis[nx][ny] || board[nx][ny] != val) continue;
 
-	while (!q.empty()) {
-		pii cur = q.front();
-		q.pop();
-
-		for (int dir = 0; dir < 4; dir++) {
-			int nx = cur.X + dx[dir];
-			int ny = cur.Y + dy[dir];
-
-			if (!OOM(nx, ny)) {
-				if (!vis[nx][ny] && groupBoard[nx][ny] == target) {
-					q.push(make_pair(nx, ny));
-					vis[nx][ny] = true;
+						q.push({ nx, ny });
+						vis[nx][ny] = true;
+					}
 				}
-				else if (!vis[nx][ny] && groupBoard[nx][ny] != target) {
-					int nxt = groupBoard[nx][ny];
-					numAdj[make_pair(target, nxt)]++;
-					numAdj[make_pair(nxt, target)]++;
-				}
+
+				cnt++;
 			}
-		}
-	}
 }
 
-void CalcArtistry()
-{
-	unordered_map<pii, int, pair_hash> duplication;
+int Artistry() {
+	int ret = 0;
 
-	for (int g1 = 0; g1 < groupNum.size(); g1++)
-	{
-		for (int g2 = g1 + 1; g2 < groupNum.size(); g2++)
-		{
-			if (duplication[make_pair(g1, g2)] > 0 || duplication[make_pair(g2, g1)] > 0) continue;
-			if (g1 == g2) continue;
+	for (int x = 0; x < N; x++)
+		for (int y = 0; y < N; y++)
+			for (int dir = 0; dir < 4; dir++) {
+				int nx = x + dx[dir];
+				int ny = y + dy[dir];
 
-			int adjCnt = numAdj[make_pair(g1, g2)];
-			if (adjCnt == 0) continue;
+				if (nx < 0 || ny < 0 || nx >= N || ny >= N) continue;
+				if (board[x][y] != board[nx][ny])
+					ret += (gnum[groupBoard[x][y]] + gnum[groupBoard[nx][ny]]) * board[x][y] * board[nx][ny];
+			}
 
-			int g1NumOfCells = numOfCells[g1];
-			int g2NumOfCells = numOfCells[g2];
-			int g1Num = groupNum[g1];
-			int g2Num = groupNum[g2];
-
-			ans += ((g1NumOfCells + g2NumOfCells) * g1Num * g2Num * adjCnt);
-			duplication[make_pair(g1, g2)]++;
-			duplication[make_pair(g2, g1)]++;
-		}
-	}
+	return ret / 2;
 }
 
-void RotateSquare(int sx, int sy, int sz)
-{
+void RotateSquare(int sx, int sy, int sz) {
 	for (int x = sx; x < sx + sz; x++)
-		for (int y = sy; y < sy + sz; y++)
-		{
-			int nx = y - sy;
-			int ny = sz - (x - sx) - 1;
-			nxtBoard[nx + sx][ny + sy] = board[x][y];
+		for (int y = sy; y < sy + sz; y++) {
+			int ox = x - sx, oy = y - sy;
+			int rx = oy, ry = sz - ox - 1;
+			nextBoard[rx + sx][ry + sy] = board[x][y];
 		}
 }
 
-void RotateBoard()
-{
-	for (int i = 0; i < N; i++) nxtBoard[N / 2][i] = board[i][N / 2];
-	for (int i = 0; i < N; i++) nxtBoard[N - i - 1][N / 2] = board[N / 2][i];
+void Rotate() {
+	int mid = N / 2;
 
-	RotateSquare(0, 0, N / 2);
-	RotateSquare(0, N / 2 + 1, N / 2);
-	RotateSquare(N / 2 + 1, 0, N / 2);
-	RotateSquare(N / 2 + 1, N / 2 + 1, N / 2);
+	for (int i = 0; i < N; i++) nextBoard[mid][i] = board[i][mid];
+	for (int i = 0; i < N; i++) nextBoard[N - i - 1][mid] = board[mid][i];
 
-	for (int i = 0; i < N; i++)
-		for (int j = 0; j < N; j++)
-			board[i][j] = nxtBoard[i][j];
+	RotateSquare(0, 0, mid);
+	RotateSquare(0, mid + 1, mid);
+	RotateSquare(mid + 1, 0, mid);
+	RotateSquare(mid + 1, mid + 1, mid);
+
+	for (int x = 0; x < N; x++)
+		for (int y = 0; y < N; y++)
+			board[x][y] = nextBoard[x][y];
 }
 
-void Solve()
-{
-	Init();
-
-	for (int i = 0; i < N; i++)
-		for (int j = 0; j < N; j++)
-			if (!vis[i][j]) MakeGroup(i, j);
-
-	fill(&vis[0][0], &vis[N][N], false);
-
-	for (int i = 0; i < N; i++)
-		for (int j = 0; j < N; j++)
-			if (!vis[i][j]) MakeAdj(i, j);
-
-	CalcArtistry();
-	RotateBoard();
-}
-
-int main(void)
-{
+int main(void) {
 	ios::sync_with_stdio(false);
 	cin.tie(NULL);
 
@@ -191,8 +123,12 @@ int main(void)
 		for (int j = 0; j < N; j++)
 			cin >> board[i][j];
 
-	for (int i = 0; i < 4; i++)
-		Solve();
+	for (int i = 0; i <= 3; i++) {
+		Init();
+		MakeGroup();
+		ans += Artistry();
+		Rotate();
+	}
 
 	cout << ans;
 	return 0;
