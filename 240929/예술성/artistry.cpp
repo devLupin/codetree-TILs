7232d420685cacf83bf9ae99cd32e2bd98892e81
Hyuntaek Lim <33558083+devLupin@users.noreply.github.com>
2024-09-29 16:27:57 +0900

    Title: 예술성, Time: 1014ms, Memory: 17MB, Status: Time Limit Exceed - CodeTree

--- 240929/예술성/artistry.cpp
+++ 240929/예술성/artistry.cpp
@@ -16,6 +16,14 @@ using namespace std;
 using pii = pair<int, int>;
 using tiii = tuple<int, int, int>;
 
+struct pair_hash
+{
+	template <class T1, class T2>
+	std::size_t operator() (const std::pair<T1, T2>& pair) const {
+		return std::hash<T1>()(pair.first) ^ std::hash<T2>()(pair.second);
+	}
+};
+
 const int dx[] = { -1,1,0,0 };
 const int dy[] = { 0,0,-1,1 };
 const int MAX_N = 30;
@@ -26,7 +34,7 @@ bool vis[MAX_N][MAX_N];
 int groupBoard[MAX_N][MAX_N];
 vector<int> numOfCells;
 vector<int> groupNum;
-vector<vector<pii>> groupPos;
+unordered_map<pii, int, pair_hash> numAdj;
 
 void Init()
 {
@@ -34,23 +42,14 @@ void Init()
 	fill(&groupBoard[0][0], &groupBoard[N][N], false);
 	numOfCells.clear();
 	groupNum.clear();
-	groupPos.clear();
+	numAdj.clear();
 }
-
-struct pair_hash
-{
-	template <class T1, class T2>
-	std::size_t operator() (const std::pair<T1, T2>& pair) const {
-		return std::hash<T1>()(pair.first) ^ std::hash<T2>()(pair.second);
-	}
-};
 
 bool OOM(int x, int y) { return x < 0 || y < 0 || x >= N || y >= N; }
 
 void MakeGroup(int x, int y)
 {
 	int cnt = 0;
-	vector<pii> pos;
 	queue<pii> q;
 	q.push(make_pair(x, y));
 
@@ -60,7 +59,6 @@ void MakeGroup(int x, int y)
 		q.pop();
 
 		groupBoard[cx][cy] = groupNum.size();
-		pos.push_back(make_pair(cx, cy));
 		vis[cx][cy] = true;
 		cnt++;
 
@@ -79,7 +77,36 @@ void MakeGroup(int x, int y)
 
 	groupNum.push_back(board[x][y]);
 	numOfCells.push_back(cnt);
-	groupPos.push_back(pos);
+}
+
+void MakeAdj(int x, int y) {
+	int target = groupBoard[x][y];
+	queue<pii> q;
+
+	q.push(make_pair(x, y));
+	vis[x][y] = true;
+
+	while (!q.empty()) {
+		pii cur = q.front();
+		q.pop();
+
+		for (int dir = 0; dir < 4; dir++) {
+			int nx = cur.X + dx[dir];
+			int ny = cur.Y + dy[dir];
+
+			if (!OOM(nx, ny)) {
+				if (!vis[nx][ny] && groupBoard[nx][ny] == target) {
+					q.push(make_pair(nx, ny));
+					vis[nx][ny] = true;
+				}
+				else if (!vis[nx][ny] && groupBoard[nx][ny] != target) {
+					int nxt = groupBoard[nx][ny];
+					numAdj[make_pair(target, nxt)]++;
+					numAdj[make_pair(nxt, target)]++;
+				}
+			}
+		}
+	}
 }
 
 void CalcArtistry()
@@ -88,28 +115,18 @@ void CalcArtistry()
 
 	for (int g1 = 0; g1 < groupNum.size(); g1++)
 	{
-		for (int g2 = 0; g2 < groupNum.size(); g2++)
+		for (int g2 = g1 + 1; g2 < groupNum.size(); g2++)
 		{
 			if (duplication[make_pair(g1, g2)] > 0 || duplication[make_pair(g2, g1)] > 0) continue;
 			if (g1 == g2) continue;
+
+			int adjCnt = numAdj[make_pair(g1, g2)];
+			if (adjCnt == 0) continue;
 
 			int g1NumOfCells = numOfCells[g1];
 			int g2NumOfCells = numOfCells[g2];
 			int g1Num = groupNum[g1];
 			int g2Num = groupNum[g2];
-			int adjCnt = 0;
-
-			for (auto pos : groupPos[g1])
-				for (int dir = 0; dir < 4; dir++)
-				{
-					int nx = pos.X + dx[dir];
-					int ny = pos.Y + dy[dir];
-
-					if (!OOM(nx, ny) && groupBoard[nx][ny] == g2)
-						adjCnt++;
-				}
-
-			if (adjCnt == 0) continue;
 
 			ans += ((g1NumOfCells + g2NumOfCells) * g1Num * g2Num * adjCnt);
 			duplication[make_pair(g1, g2)]++;
@@ -120,7 +137,7 @@ void CalcArtistry()
 
 void RotateSquare(int sx, int sy, int sz)
 {
-	for(int x = sx; x < sx + sz; x++)
+	for (int x = sx; x < sx + sz; x++)
 		for (int y = sy; y < sy + sz; y++)
 		{
 			int nx = y - sy;
@@ -144,7 +161,7 @@ void RotateBoard()
 			board[i][j] = nxtBoard[i][j];
 }
 
-void Solve(int cnt)
+void Solve()
 {
 	Init();
 
@@ -152,10 +169,14 @@ void Solve(int cnt)
 		for (int j = 0; j < N; j++)
 			if (!vis[i][j]) MakeGroup(i, j);
 
-	CalcArtistry();
+	fill(&vis[0][0], &vis[N][N], false);
 
-	if(cnt < 3)
-		RotateBoard();
+	for (int i = 0; i < N; i++)
+		for (int j = 0; j < N; j++)
+			if (!vis[i][j]) MakeAdj(i, j);
+
+	CalcArtistry();
+	RotateBoard();
 }
 
 int main(void)
@@ -170,8 +191,8 @@ int main(void)
 		for (int j = 0; j < N; j++)
 			cin >> board[i][j];
 
-	for(int i=0; i<4; i++)
-		Solve(i);
+	for (int i = 0; i < 4; i++)
+		Solve();
 
 	cout << ans;
 	return 0;
